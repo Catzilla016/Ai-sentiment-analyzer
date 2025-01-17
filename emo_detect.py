a310a8634d8f4c2045cabd9ae9530fb66c3c9854
Catzilla016 <alexiosg555@gmail.com>
2025-01-17 18:34:32 -0500

    Working emotion detector function

--- emo_detect.py
+++ emo_detect.py
@@ -2,10 +2,31 @@ import requests
 import json
 
 def emotion_detector(text_to_analyse): 
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
-    header =  {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-    input_json =  { "raw_document": { "text": text_to_analyse } }
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyse } }
 
-    response = requests.post(url, json = myobj, headers=header)
+    response = requests.post(url, json=input_json, headers=header)
 
-    return response.text  
+    formatted_response = json.loads(response.text)
+
+    all_emotions = formatted_response["emotionPredictions"][0]["emotion"]
+
+    anger_score = all_emotions["anger"]
+    disgust_score = all_emotions["disgust"]
+    fear_score = all_emotions["fear"]
+    joy_score = all_emotions["joy"]
+    sadness_score = all_emotions["sadness"]
+
+    dom_emotion = max(all_emotions, key=all_emotions.get)
+
+    emotion_dict = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dom_emotion
+        }
+    
+    return emotion_dict
